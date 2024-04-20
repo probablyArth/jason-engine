3c29fdf1be46b17459d32afa15ab485218002fdb
Arth <probablyarth@gmail.com>
2024-04-21 00:06:34 +0530

    update linters and tsconifg to support absolute paths

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'eslint:recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: 'eslint.tsconfig.json',
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-unused-expressions': 'error',
+    'no-var': 'error',
+    semi: 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-multi-spaces': 'error',
+    'space-in-parens': 'error',
+    'no-multiple-empty-lines': 'error',
+    'prefer-const': 'error',
+    quotes: [2, 'single', { avoidEscape: true }],
+  },
+  overrides: [
+    {
+      files: ['__tests__/**/*.ts'],
+      env: { jest: true, node: true },
+    },
+  ],
+};
